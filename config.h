0fc08065b46d8d35af19bb37138f18daa2c092b3
awcot <awcot.dev@gmail.com>
2020-12-20 17:39:22 +0000

    Font height to better accommodate noto

--- config.h
+++ config.h
@@ -8,11 +8,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Noto Sans:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Noto Sans:pixelsize=16:antialias=true:autohint=true";
-static const char col_bg[]          = "#44475a";
+static const char col_bg[]          = "#292c34";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_accent[]      = "#ff5555";
+static const char col_accent[]      = "#e06c75";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_bg, col_gray2 },
