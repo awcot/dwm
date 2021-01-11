2ea71d11fbae60f281f9553333beabc9e4607a7b
awcot <awcot.dev@gmail.com>
2021-01-11 19:29:19 +0000

    Added the titlecolor patch

--- config.h
+++ config.h
@@ -15,8 +15,9 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_accent[]      = "#e06c75";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_bg, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_bg,  col_accent  },
+	[SchemeNorm] = { col_gray4, col_bg, col_gray2 },
+	[SchemeSel]  = { col_accent, col_bg,  col_accent  },
+	[SchemeTitle]  = { col_gray4, col_bg,  col_bg  },
 };
 
 /* tagging */
