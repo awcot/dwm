dcbb2a8a8b1d7628ef77525b9a4a12ef8f4a3e9e
awcot <awcot.dev@gmail.com>
2021-01-23 12:33:57 +0000

    Colour bg selected tag

--- config.h
+++ config.h
@@ -19,12 +19,13 @@ static const char col_accent2[]     = "#61afef";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm]  = { col_gray4, col_bg, col_gray1 },
-	[SchemeSel]   = { col_accent1, col_bg,  col_accent1  },
+	[SchemeSel]   = { col_gray4, col_accent1,  col_accent1  },
 	[SchemeTitle] = { col_gray4, col_bg,  col_bg  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "", "", "", "4", "5", "6", "7", "", "" }; */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
